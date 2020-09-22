73c4993b2ae13da07cb5253a04f02494c65a816d
azrinurvani <azrinurvani@gmail.com>
2020-09-22 14:42:28 +0700

    Add action for All button Animation

--- app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
+++ app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -16,10 +16,19 @@
 
 package com.google.samples.propertyanimation
 
+import android.animation.*
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.LinearInterpolator
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.animation.addListener
 
 
 class MainActivity : AppCompatActivity() {
@@ -69,22 +78,117 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Extension function for ObjectAnimator
+    private fun ObjectAnimator.disableViewDuringAnimation(view : View, animator : ObjectAnimator){
+        animator.addListener(object: AnimatorListenerAdapter(){
+            override fun onAnimationStart(animation: Animator?, isReverse: Boolean) {
+                view.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) {
+                view.isEnabled = true
+            }
+        })
+    }
+
     private fun rotater() {
+        val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)//posisi rotasi dari object yang di rotate, 0f maka object akan berada pada posisi semula sebelum di rotate
+        animator.duration = 1000 //in milli seconds
+        //TODO : Add a new AnimatorListenerAdapter object to the animator
+        // and override the onAnimationStart() and onAnimationEnd() methods.
+        /*animator.addListener(object: AnimatorListenerAdapter(){
+            override fun onAnimationStart(animation: Animator?) {
+                rotateButton.isEnabled = false;
+            }
+
+            override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) {
+                rotateButton.isEnabled = true;
+            }
+        })*/
+
+        animator.disableViewDuringAnimation(rotateButton,animator)
+        animator.start()
+
     }
 
     private fun translater() {
+        val animator = ObjectAnimator.ofFloat(star, View.TRANSLATION_X,200f) //values 200f menentukan jarak dari object bergeser
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(translateButton,animator)
+        animator.start()
     }
 
+
     private fun scaler() {
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X,4f) //object melebar kesamping
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y,4f)// object melebar keatas/kebawah/vertical
+        val animator = ObjectAnimator.ofPropertyValuesHolder(star,scaleX,scaleY)
+
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(scaleButton,animator)
+        animator.start()
     }
 
     private fun fader() {
+        val animator = ObjectAnimator.ofFloat(star, View.ALPHA,0f)
+        animator.repeatCount =1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(fadeButton,animator)
+        animator.start()
     }
 
     private fun colorizer() {
+        var animator = ObjectAnimator.ofArgb(star.parent,"backgroundColor", Color.WHITE)
+        /*Note: The ofArgb() method is the reason that this app builds against minSdk 21;
+         the rest of the functionality of the app can be run on earlier SDKs, but ofArgb() was introduced in the Lollipop release.
+         It is also possible to animate color values on earlier releases, involving TypeEvaluators,
+         and the use of ArgbEvaluator specifically. We used ofArgb() in this lesson instead for simplicity.*/
+
+        animator.duration = 500
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(colorizeButton,animator)
+        animator.start()
+
     }
 
     private fun shower() {
+        val container       = star.parent as ViewGroup
+        val containerW      = container.width
+        val containerH      = container.height
+        var starW : Float   = star.width.toFloat()
+        var starH : Float   = star.height.toFloat()
+
+        val newStar = AppCompatImageView(this)
+        newStar.setImageResource(R.drawable.ic_star)
+        newStar.layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
+                                                        FrameLayout.LayoutParams.WRAP_CONTENT)
+
+        container.addView(newStar)
+
+        newStar.scaleX = Math.random().toFloat() * 1.5f + 1f
+        newStar.scaleY = newStar.scaleX
+        starW = newStar.scaleX
+        starH = newStar.scaleY
+        newStar.translationX = Math.random().toFloat() * containerW - starW/2
+
+        val mover = ObjectAnimator.ofFloat(newStar,View.TRANSLATION_Y,-starH,containerH+starH)
+        mover.interpolator = AccelerateInterpolator(1f)
+        val rotator = ObjectAnimator.ofFloat(newStar,View.ROTATION,(Math.random() * 1080).toFloat())
+        rotator.interpolator = LinearInterpolator()
+
+        val set = AnimatorSet()
+        set.playTogether(mover,rotator)
+        set.duration = (Math.random()* 1500 + 500).toLong()
+        set.addListener(object: AnimatorListenerAdapter(){
+            override fun onAnimationEnd(animation: Animator?, isReverse: Boolean) {
+                container.removeView(newStar)
+            }
+        })
+        set.start()
+
     }
 
 }
